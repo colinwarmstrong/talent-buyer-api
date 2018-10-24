caa3fa0d0caf2a1309778f680fbea83149376fcb
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-10-24 16:15:30 -0600

    Push to test frontend signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   # include ActionController::MimeResponds
-
+  protect_from_forgery with: :null_session
   respond_to :json
 
   before_action :configure_permitted_parameters, if: :devise_controller?
