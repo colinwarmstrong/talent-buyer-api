d926abe913b38e9a174c037dcf2066330bddb962
Colin Armstrong <colinwarmstrong@gmail.com>
2018-10-24 20:27:03 -0600

    Test sad path for creating offers

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::OffersController < ApplicationController
   def create
     offer = Offer.create(offer_params)
-    if offer
+    if offer.id
       render json: offer, status: 201
     else
-      render json: { message: 'Invalid offer parameters' }, status: 400
-    end 
+      render json: { message: 'Invalid offer parameters.' }, status: 400
+    end
   end
 
   private
 
   def offer_params
-    params.require(:offer).permit(:show_id, :artist_id, :guarantee, :bonuses,
+    params.permit(:show_id, :artist_id, :guarantee, :bonuses,
                                   :radius_clause, :total_expenses, :gross_potential,
                                   :door_times, :age_range, :merch_split)
   end
--- spec/requests/api/v1/offers/a_buyer_can_create_an_offer_spec.rb
+++ spec/requests/api/v1/offers/a_buyer_can_create_an_offer_spec.rb
@@ -3,21 +3,19 @@ require 'rails_helper'
 describe 'POST /api/v1/offers' do
   context 'a buyer creating an offer' do
     it 'will be attached to a show and venue' do
-      show = Fabricate(:show)
+      show    = Fabricate(:show)
       artist = Fabricate(:artist)
 
-      offer_payload = { offer: {
-                          show_id: show.id,
-                          artist_id: artist.id,
-                          guarantee: 10000,
-                          bonuses: 'No bonuses',
-                          radius_clause: 'No shows within 100 miles ever',
-                          total_expenses: 12000,
-                          gross_potential: 15000,
-                          door_times: '9pm-12pm',
-                          age_range: 'Over 21',
-                          merch_split: '90-10'
-                        }
+      offer_payload = { show_id: show.id,
+                        artist_id: artist.id,
+                        guarantee: 10000,
+                        bonuses: 'No bonuses',
+                        radius_clause: 'No shows within 100 miles ever',
+                        total_expenses: 12000,
+                        gross_potential: 15000,
+                        door_times: '9pm-12pm',
+                        age_range: 'Over 21',
+                        merch_split: '90-10'
                       }
 
       expect(Offer.count).to eq(0)
@@ -32,16 +30,45 @@ describe 'POST /api/v1/offers' do
       new_offer = JSON.parse(response.body, symbolize_names: true)
 
       expect(new_offer).to have_key(:id)
-      expect(new_offer[:show_id]).to eq(offer_payload[:offer][:show_id])
-      expect(new_offer[:artist_id]).to eq(offer_payload[:offer][:artist_id])
-      expect(new_offer[:guarantee]).to eq(offer_payload[:offer][:guarantee])
-      expect(new_offer[:bonuses]).to eq(offer_payload[:offer][:bonuses])
-      expect(new_offer[:radius_clause]).to eq(offer_payload[:offer][:radius_clause])
-      expect(new_offer[:total_expenses]).to eq(offer_payload[:offer][:total_expenses])
-      expect(new_offer[:gross_potential]).to eq(offer_payload[:offer][:gross_potential])
-      expect(new_offer[:door_times]).to eq(offer_payload[:offer][:door_times])
-      expect(new_offer[:age_range]).to eq(offer_payload[:offer][:age_range])
-      expect(new_offer[:merch_split]).to eq(offer_payload[:offer][:merch_split])
+      expect(new_offer[:show_id]).to eq(offer_payload[:show_id])
+      expect(new_offer[:artist_id]).to eq(offer_payload[:artist_id])
+      expect(new_offer[:guarantee]).to eq(offer_payload[:guarantee])
+      expect(new_offer[:bonuses]).to eq(offer_payload[:bonuses])
+      expect(new_offer[:radius_clause]).to eq(offer_payload[:radius_clause])
+      expect(new_offer[:total_expenses]).to eq(offer_payload[:total_expenses])
+      expect(new_offer[:gross_potential]).to eq(offer_payload[:gross_potential])
+      expect(new_offer[:door_times]).to eq(offer_payload[:door_times])
+      expect(new_offer[:age_range]).to eq(offer_payload[:age_range])
+      expect(new_offer[:merch_split]).to eq(offer_payload[:merch_split])
+    end
+
+    it 'will not create an offer if given invalid parameters' do
+      show    = Fabricate(:show)
+      artist = Fabricate(:artist)
+
+      offer_payload = { show_id: show.id,
+                        artist_id: artist.id,
+                        guarantee: 10000,
+                        bonuses: 'No bonuses',
+                        radius_clause: 'No shows within 100 miles ever',
+                        total_expenses: 12000,
+                        gross_potential: 15000,
+                        door_times: '9pm-12pm',
+                        age_range: 'Over 21'
+                      }
+
+      expect(Offer.count).to eq(0)
+
+      post '/api/v1/offers', params: offer_payload.to_json, headers: {'Content-Type': 'application/json'}
+
+      expect(Offer.count).to eq(0)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      invalid_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invalid_response[:message]).to eq('Invalid offer parameters.')
     end
   end
 end
