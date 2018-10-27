17056fea0c775024799d043487c4df26f5d189ee
Colin Armstrong <colinwarmstrong@gmail.com>
2018-10-26 20:00:01 -0600

    Add endpoint to get all artists

--- spec/requests/api/v1/artists/artist_queries_spec.rb
+++ spec/requests/api/v1/artists/artist_queries_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
 
 describe 'Arist Endpoints' do
+  context '/api/v1/artists' do
+    it 'returns all artists' do
+      artist_1 = Fabricate(:artist, name: 'Beastie Bois')
+      artist_2 = Fabricate(:artist, name: 'Beach Bois')
+      artist_3 = Fabricate(:artist, name: 'New Bois')
+      artist_4 = Fabricate(:artist, name: 'Franchise Bois')
+
+      get '/api/v1/artists'
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      artists = JSON.parse(response.body, symbolize_names: true)
+      artist = artists.first
+
+      expect(artists).to be_an(Array)
+      expect(artists.length).to eq(4)
+
+      expect(artist[:name]).to eq(artist_1.name)
+      expect(artist[:agency]).to eq(artist_1.agency)
+      expect(artist[:songkick_id]).to eq(artist_1.songkick_id)
+      expect(artist[:popularity]).to eq(artist_1.popularity)
+      expect(artist[:image_url]).to eq(artist_1.image_url)
+      expect(artist[:spotify_id]).to eq(artist_1.spotify_id)
+      expect(artist[:spotify_url]).to eq(artist_1.spotify_url)
+      expect(artist[:spotify_followers]).to eq(artist_1.spotify_followers)
+    end
+  end
+
   context '/api/v1/artists/?genre=:genre' do
     it 'returns all artists by specified genre' do
       artist_1 = Fabricate(:artist, name: 'Beastie Bois')
