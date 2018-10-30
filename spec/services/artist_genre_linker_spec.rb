78f1e990658ab66f04ff7629c1a216c7ac628b96
jtrtj <jt.r@live.com>
2018-10-30 11:16:29 -0600

    Add test to ArtistGenreLinker spec to check for adding duplicate genres

--- spec/services/artist_genre_linker_spec.rb
+++ spec/services/artist_genre_linker_spec.rb
@@ -10,6 +10,13 @@ describe ArtistGenreLinker do
       expect(artist.genres.count).to eq(3)
     end
 
-    # it '#link - will not '
+    it '#link - will not create new genres if they all ready exist' do
+      artist = Fabricate(:artist, popularity: 100, name: 'Linkin Park')
+      cool_genre = Genre.create!(name: "claire's favorite band")
+      genres = "Rock, Rap-Rock, Claire's favorite band"
+
+      expect{ArtistGenreLinker.link(artist, genres)}.to change{Genre.count}.by(2)
+      expect(artist.genres.count).to eq(3)
+    end
   end
 end
