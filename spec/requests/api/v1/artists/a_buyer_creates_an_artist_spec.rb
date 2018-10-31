46c41f78d67d1eb27c0d60ef72240ed9b7d3bf0a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-10-31 16:47:57 -0600

    Add test to ensure multiple artists with same id not created

--- spec/requests/api/v1/artists/a_buyer_creates_an_artist_spec.rb
+++ spec/requests/api/v1/artists/a_buyer_creates_an_artist_spec.rb
@@ -41,6 +41,41 @@ describe 'POST /api/v1/artists' do
       expect(new_artist[:spotify_url]).to eq(artist_payload[:spotify_url])
     end
 
+    it 'will only alter an existing artist if songkick_id already in the database' do
+      artist = Fabricate(:artist, songkick_id: 100)
+
+      existing_artist_payload = {  name: 'Yung Boi',
+                          agency: 'Warner Bros. Entertainment',
+                          songkick_id: 100,
+                          image_url: 'http://amazon.com',
+                          popularity: 78,
+                          spotify_id: '101',
+                          spotify_url: 'www.example.com',
+                          spotify_followers: 101,
+                          genres: 'Rock, Reggae'
+                        }
+
+      expect(Artist.count).to eq(1)
+
+      post '/api/v1/artists', params: existing_artist_payload.to_json, headers: {'Content-Type': 'application/json'}
+
+      expect(Artist.count).to eq(1)
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      existing_artist = JSON.parse(response.body, symbolize_names: true)
+
+      expect(existing_artist).to have_key(:id)
+      expect(existing_artist[:name]).to eq(existing_artist_payload[:name])
+      expect(existing_artist[:agency]).to eq(existing_artist_payload[:agency])
+      expect(existing_artist[:songkick_id]).to eq(existing_artist_payload[:songkick_id])
+      expect(existing_artist[:spotify_id]).to eq(existing_artist_payload[:spotify_id])
+      expect(existing_artist[:image_url]).to eq(existing_artist_payload[:image_url])
+      expect(existing_artist[:popularity]).to eq(existing_artist_payload[:popularity])
+      expect(existing_artist[:spotify_followers]).to eq(existing_artist_payload[:spotify_followers])
+      expect(existing_artist[:spotify_url]).to eq(existing_artist_payload[:spotify_url])
+    end
+
     it 'will not be created if parameters incorrect' do
       artist_payload = {  name: 'Yung Boi',
                           agency: 'Whup'
